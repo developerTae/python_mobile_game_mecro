2679b87b8047e605566a9becc0f393c34aa7192e
Tae Sung Park <46318721+korean-handsome@users.noreply.github.com>
2020-08-20 01:05:42 +0900

    Update main.py
    Fit to personal PC settings

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import pyautogui as pag
 import mss, cv2
 import numpy as np
 import helper
 
 # bluestacks 좌표
 # 아이콘 위치
-left_icon_pos = {'left': 190, 'top': 1051, 'width': 130, 'height': 130}
-right_icon_pos = {'left': 496, 'top': 1051, 'width': 130, 'height': 130}
+left_icon_pos = {'left': 793, 'top': 725, 'width': 120, 'height': 120}
+right_icon_pos = {'left': 1011, 'top': 719, 'width': 120, 'height': 120}
 
 # 버튼 위치
-left_button = [129, 1321]
-right_button = [696, 1328]
+left_button = [696, 856]
+right_button = [1105, 826]
 
 while True:
 
@@ -17,4 +18,27 @@ while True:
         left_img = np.array(sct.grab(left_icon_pos))[:, :, :3]
         right_img = np.array(sct.grab(right_icon_pos))[:, :, :3]
         helper.mecro(left_img, right_img, left_button, right_button)
+
+# 아이콘 위치를 잘 구했는지 확인
+# with mss.mss() as sct:
+#     left_img = np.array(sct.grab(left_icon_pos))[:,:,:3]
+#     right_img = np.array(sct.grab(right_icon_pos))[:,:,:3]
+#
+#     cv2.imshow('left_img',left_img)
+#     cv2.imshow('right_img',right_img)
+#     cv2.waitKey(0)
+#
+# while True:
+#     x, y = pag.position()
+#     position_str = 'X: ' + str(x) + 'Y: ' + str(y)
+#     print(position_str)
+
+# 마우스 좌표값 구하기
+# import pyautogui as pag
+#
+# while True:
+#     x, y = pag.position()
+#     position_str = 'X: ' + str(x) + 'Y: ' + str(y)
+#     print(position_str)
+
 
